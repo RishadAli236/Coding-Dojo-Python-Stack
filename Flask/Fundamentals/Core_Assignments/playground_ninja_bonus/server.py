9b287dd36ce530fda54e5a117f0888308ccfc412
Rishad Yasin <rishad236@gmail.com>
2023-04-22 18:51:35 -0700

    attached multiple routes to one function

--- Flask/Fundamentals/Core_Assignments/playground_ninja_bonus/server.py
+++ Flask/Fundamentals/Core_Assignments/playground_ninja_bonus/server.py
@@ -4,16 +4,11 @@ app = Flask(__name__)
 
 
 @app.route("/play")
-def play(x = 3, color = "aqua"): # Assign default values to x and color
-    return render_template("index.html", num = x, box_color = color)
-
 @app.route("/play/<x>") 
-def play_x(x = 3, color = "aqua"):
+@app.route("/play/<x>/<color>")
+def index(x = 3, color = "aqua"): # Assign default values to x and color
     return render_template("index.html", num = int(x), box_color = color)
 
-@app.route("/play/<x>/<color>")
-def play_x_color(x = 3, color = "aqua"):
-    return render_template("index.html", num = int(x), box_color = color)
 
 if __name__ == "__main__":
     app.run(debug = True)
