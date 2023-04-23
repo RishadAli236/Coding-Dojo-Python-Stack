ceed19d1d55461c24e924d1965bb7a475fc0a682
Rishad Yasin <rishad236@gmail.com>
2023-04-22 18:12:49 -0700

    added comment

--- Flask/Fundamentals/Core_Assignments/playground_ninja_bonus/server.py
+++ Flask/Fundamentals/Core_Assignments/playground_ninja_bonus/server.py
@@ -2,16 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/play")
-def play(x = 3, color = "aqua"):
+def play(x = 3, color = "aqua"): # Assign default values to x and color
     return render_template("index.html", num = x, box_color = color)
 
-@app.route("/play/<x>")
-def play_x(x, color = "aqua"):
+@app.route("/play/<x>") 
+def play_x(x = 3, color = "aqua"):
     return render_template("index.html", num = int(x), box_color = color)
 
 @app.route("/play/<x>/<color>")
-def play_x_color(x, color):
+def play_x_color(x = 3, color = "aqua"):
     return render_template("index.html", num = int(x), box_color = color)
 
 if __name__ == "__main__":
