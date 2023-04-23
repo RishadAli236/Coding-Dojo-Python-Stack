4027cba4e24d1c31432641b902ff63d593a4efa1
Rishad Yasin <rishad236@gmail.com>
2023-04-22 17:59:38 -0700

    added playground_ninja_bonus

--- Flask/Fundamentals/Core_Assignments/playground/server.py
+++ Flask/Fundamentals/Core_Assignments/playground/server.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
 @app.route("/play")
 def play():
     return render_template("play.html")
---
+++ Flask/Fundamentals/Core_Assignments/playground_ninja_bonus/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/play")
+def play(x = 3, color = "aqua"):
+    return render_template("index.html", num = x, box_color = color)
+
+@app.route("/play/<x>")
+def play_x(x, color = "aqua"):
+    return render_template("index.html", num = int(x), box_color = color)
+
+@app.route("/play/<x>/<color>")
+def play_x_color(x, color):
+    return render_template("index.html", num = int(x), box_color = color)
+
+if __name__ == "__main__":
+    app.run(debug = True)
